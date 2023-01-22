a0d6598fdcd0f666ece13abe49afc2d7a2f8a507
EngAliSaleh <al3qrabxxo@gmail.com>
2023-01-22 10:01:48 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-let search = document.querySelector('#nav .navmdl .container .icon .search')
-let form = document.querySelector('#nav .navmdl .container .form')
+let regiest = document.getElementById('regiest');
+let navMobile = document.getElementById('mobile-categories');
+let visa_input = document.getElementsByClassName('visa');
+let visamethod = document.getElementById('payment-method');
+
+
+
+regiest.onclick = function (){
+    navMobile.classList.toggle ('show');  
+}
+
+visa_input.onclick = function (){
+    visamethod.classList.toggle ('show');  
+}
+
+
 
