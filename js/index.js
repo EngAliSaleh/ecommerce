dadc30d16836d300d8ad88eda8c705c7d4fc61c2
EngAliSaleh <al3qrabxxo@gmail.com>
2023-01-25 12:59:54 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -1,22 +1,35 @@
 let regiest = document.getElementById('regiest');
-let categories = document.getElementById('categories');
 let navMobile = document.getElementById('mobile-registary');
+let categories = document.getElementById('categories');
 let navMobilecategories = document.getElementById('nav-mobile-categories');
+//////
+let orderradio = document.getElementById('order-radio') || "";
+let orderdiv = document.getElementById('order-div') || "";
+//////
+//////
+let userdiv = document.getElementById('user');
+let userlist = document.getElementById('user-list');
+//////
 let visa_input = document.querySelector('label[for="visa"]');
-console.log(visa_input);
 let visamethod = document.querySelector('#payment-method .visa');
 
 
 
-regiest.onclick = function (){
-    navMobile.classList.toggle ('show');  
+regiest.onclick = function () {
+    navMobile.classList.toggle('show');
 }
-categories.onclick = function (){
-    navMobilecategories.classList.toggle ('show');  
+categories.onclick = function () {
+    navMobilecategories.classList.toggle('show');
+}
+orderdiv.onclick = function () {
+    orderradio.classList.toggle('show');
+}
+userdiv.onclick = function () {
+    userlist.classList.toggle('show');
 }
 
-visa_input.onclick = function (){
-    visamethod.classList.toggle ('show');  
+visa_input.onclick = function () {
+    visamethod.classList.toggle('show');
 }
 
 
